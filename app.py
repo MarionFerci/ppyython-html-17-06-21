990a0cf32c3cd317be37a411bbe23eef94742cea
MarionFerci <ferreiracintiasoa@gmail.com>
2021-06-17 19:16:21 +0000

    teste

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index(): 
-    return "Hello World"
+    return "Hello World 2"
+
+@app.route('/pastel')
+def pastel(): 
+    return "Meu Pastel é mais Barato"
+
+@app.route('/pastel/detalhes')
+def pastel_detalhe(): 
+    return "5000" 
